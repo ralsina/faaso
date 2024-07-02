a4f722a1e08d2f483727472388afc23c9840172e
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-02 16:56:19 -0300

    Use actual funkos in the admin backend

--- src/daemon/funkos.cr
+++ src/daemon/funkos.cr
@@ -1,25 +1,11 @@
 require "docr"
 require "kemal"
+require "../funko.cr"
 
 module Funkos
-  struct Funko
-    include JSON::Serializable
-    property name : String
-
-    def initialize(@name : String)
-    end
-  end
-
   get "/funkos/" do |env|
-    docker_api = Docr::API.new(Docr::Client.new)
-    containers = docker_api.containers.list(all: true)
+    funkos : Array(Funko) = Funko.from_docker
 
-    funkos = [] of Funko
-    containers.each { |container|
-      names = container.names.select &.starts_with? "/faaso-"
-      next if names.empty?
-      funkos << Funko.new(name: names[0][7..])
-    }
     funkos.sort! { |a, b| a.name <=> b.name }
 
     if env.params.query.fetch("format", "json") == "html"
