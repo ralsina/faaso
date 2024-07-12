e217e0693253abfe201b4f4cfcc0b2d95730b7db
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-12 20:58:34 -0300

    Fix all secret interaction in the frontend

--- src/daemon/secrets.cr
+++ src/daemon/secrets.cr
@@ -7,9 +7,11 @@ module Secrets
 
   # Gets a secret in form {"name": "funko_name-secret_name", "value": "secret_value"}
   post "/secrets/" do |env|
+    pp! env.params
     funko = env.params.body["funko"].as(String)
     name = env.params.body["name"].as(String)
     value = env.params.body["value"].as(String)
+    Log.info { "Creating secret #{funko}-#{name}" }
     if funko.empty? || name.empty? || value.empty?
       halt env, status_code: 400, response: "Bad request"
     end
@@ -29,6 +31,27 @@ module Secrets
     if env.params.query.fetch("format", "json") == "html"
       render "src/views/secrets.ecr"
     else
+      result.to_json
+    end
+  end
+
+  # Deletes a secret from the disk and memory
+  get "/secrets/:funko/:name/" do |env|
+    funko = env.params.url["funko"]
+    name = env.params.url["name"]
+
+    if funko == name == "-"
+      result = {"funko" => "", "name" => ""}
+    else
+      # We never give up the secrets value over HTTP
+      result = {"funko" => funko,
+                "name"  => name,
+      }
+    end
+
+    if env.params.query.fetch("format", "json") == "html"
+      render "src/views/secret_dialog.ecr"
+    else # This is pretty useless ;-)
       result.to_json
     end
   end
