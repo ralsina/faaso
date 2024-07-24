96c650be62ac797334bdbc2d6af4d09cd85793d3
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-24 13:42:19 -0300

    Working example

--- examples/express/historico/funko.js
+++ examples/express/historico/funko.js
@@ -1,32 +1,58 @@
-const { Client } = require("pg");
+const asyncHandler = require("express-async-handler");
 const express = require("express");
+const { Client } = require("pg");
 const fs = require("node:fs");
 const app = express();
 const port = 3000;
 
-const client = new Client({
-  user: "postgres", // fs.readFileSync("/secrets/user"),
-  password: "postgres", // fs.readFileSync("/secrets/pass"),
-  host: "localhost",
-  database: "nombres",
-});
+USER = fs.readFileSync("/secrets/user"),
+PASS = fs.readFileSync("/secrets/pass"),
 
-app.get("/", (req, res) => {
-  client.connect().then(() => {
-    client.query("SELECT * FROM nombres limit 5").then((err, result) => {
-      if (err) {
-        console.error(err);
-      } else {
-        res.send(result.rows);
-      }
-      client.end();
+app.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    names = req.query.names;
+    if (!names) {
+      res.send("No names provided");
+      return;
+    }
+    names = names.split(",").map((n) => n.trim());
+
+    const client = new Client({
+      user: USER,
+      password: PASS,
+      host: "localhost",
+      database: "nombres",
     });
-  });
-});
+
+    var response = [];
+    response.push(["Año", ...names]);
+    for (year = 1922; year < 2016; year++) {
+      row = [year, 0, 0];
+      response.push(row);
+    }
+
+    await client.connect();
+    for (const [i, name] of names.entries()) {
+      const result = await client.query(
+        "SELECT anio,contador FROM nombres where nombre = $1",
+        [name]
+      );
+      console.log("filling response");
+      for (row of result.rows) {
+        response[row.anio - 1921][i + 1] = row.contador;
+      }
+    }
+    console.log("responding");
+    res.json(response);
+  })
+);
 
 app.get("/ping", (req, res) => {
   res.send("OK");
 });
+
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Example funko listening on port ${port}`);
