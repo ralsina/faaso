9e36d4989da9c339345f30ad799014a6aa1df41f
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-24 17:34:11 -0300

    Minor improvement in the static HTML page for historico

--- upload-docker.sh
+++ upload-docker.sh
@@ -3,5 +3,5 @@ pass github-registry | docker login ghcr.io -u ralsina --password-stdin
 docker run --rm --privileged \
         multiarch/qemu-user-static \
         --reset -p yes
-docker build . --platform=linux/arm64 -t ghcr.io/ralsina/faaso-arm64:latest --push
-docker build . --platform=linux/amd64 -t ghcr.io/ralsina/faaso:latest --push
+docker build . --platform=linux/arm64 -t ghcr.io/ralsina/faaso-arm64:latest -t ghcr.io/ralsina/faaso-arm64:0.1.0 --push
+docker build . --platform=linux/amd64 -t ghcr.io/ralsina/faaso:latest -t ghcr.io/ralsina/faaso:0.1.0 --push
