757a8173752cbf9855a5a8a1a748f32dcaf75371
Roberto Alsina <roberto.alsina@gmail.com>
2025-04-14 16:32:14 -0300

    bump: Release v0.2.0

--- do_release.sh
+++ do_release.sh
@@ -6,7 +6,6 @@ VERSION=$(git cliff --bumped-version --unreleased |cut -dv -f2)
 
 sed "s/^version:.*$/version: $VERSION/g" -i shard.yml
 git add shard.yml
-hace lint test
 git cliff --bump -u -p CHANGELOG.md
 git commit -a -m "bump: Release v$VERSION"
 make static
